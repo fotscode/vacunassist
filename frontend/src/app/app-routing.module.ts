830b02057d764c6d90335e75f41a6bf3356d05de
Ian Petraccaro Cantero <ianpetraccaro@gmail.com>
2022-05-03 08:48:02 -0300

    Added error, login and register page modules

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 const routes: Routes=[
   {path: 'Home', component:HomeComponent},
+  {path: 'Login', component:LoginPageComponent},
+  {path: 'Sign_up', component:RegisterPageComponent},
   {path: '', redirectTo:'/Home', pathMatch:'full'},
+  {path: '**', pathMatch:'full', component:ErrorPageComponent}
 ]
 
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavigationHeaderComponent } from './navigation-header/navigation-header.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationHeaderComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
