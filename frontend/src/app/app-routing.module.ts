bd205a2803ed32b896384a959b4073bc200e6c05
fots <facundotomatis@gmail.com>
2022-05-12 04:11:39 -0300

    removed title and unused components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
+
+// components
 import { HomeComponent } from './components/home/home.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { ErrorPageComponent } from './components/error-page/error-page.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
 
 const routes: Routes=[
-  {path: 'Home', component:HomeComponent,
-    data:{  title: 'Home'
-    }
-  },
+  {path: 'Home', component:HomeComponent},
   {path: 'Login', component:LoginPageComponent},
   {path: 'Register', component:RegisterPageComponent},
   {path: '', redirectTo:'/Home', pathMatch:'full'},
