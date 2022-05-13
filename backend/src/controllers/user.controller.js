32002977b50107c1103670ceb211ad5ad2b62cf6
fots <facundotomatis@gmail.com>
2022-05-13 05:30:56 -0300

    now it sends the password by email

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,13 +1,14 @@
-const crypto=require('crypto')
+const crypto = require('crypto')
 const User = require('../models/User')
-const utils=require('../lib/utils')
+const utils = require('../lib/utils')
+const nodemailer = require('nodemailer')
 //TODO CRUD operations
 // async/await for db operations
 exports.signUp = (req, res, next) => {
   User.findOne({ cuil: req.body.cuil }).then((user) => {
     if (!user) {
       // if user not found, create user, else inform error
-      const pwd=crypto.randomBytes(16).toString('hex')
+      const pwd = crypto.randomBytes(16).toString('hex')
       const saltHash = utils.genPassword(pwd)
       const hash = saltHash.hash
       const salt = saltHash.salt
@@ -18,17 +19,16 @@ exports.signUp = (req, res, next) => {
         cuil: req.body.cuil,
         riesgo: req.body.riesgo,
         sede: req.body.sede,
-        role:req.body.role,
+        role: req.body.role,
         hash: hash,
         salt: salt,
       })
-      //TODO ELIMINAR ESTO cuando se mande el mail con la password
-      console.log("Contrasenia: "+ pwd)
       newUser
         .save()
         .then((user) => {
+          sendEmail(req.body.email,pwd)
           const jwt = utils.issueJWT(user)
-          res.json({
+          res.status(200).json({
             success: true,
             user: user,
             token: jwt.token,
@@ -37,5 +37,28 @@ exports.signUp = (req, res, next) => {
         })
         .catch((err) => next(err))
     } else res.status(409).json({ success: false, msg: 'CUIL ya registrado' })
+  })
+}
+
+const sendEmail = (email, pwd) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'vacunassistG26@gmail.com',
+      pass: 'vacunassistG26pwd',
+    },
+  })
+  const mailOptions = {
+    from: 'vacunassistG26@gmail.com',
+    to: email,
+    subject: 'Vacunassist - Contraseña',
+    text: 'Contraseña: ' + pwd,
+  }
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('Email enviado' + info.response)
+    }
   })
 }
