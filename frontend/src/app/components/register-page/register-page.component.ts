263cef3ea0b11cc6da9adb138540e094ed94861d
Relderf <56208840+Relderf@users.noreply.github.com>
2022-05-17 20:49:33 -0300

    Registrar tiene "riesgo" y "sedes" como en el "ver perfil"

--- frontend/src/app/components/register-page/register-page.component.ts
+++ frontend/src/app/components/register-page/register-page.component.ts
@@ -3,21 +3,34 @@ import { AuthService } from '../../services/auth.service'
 import { Router } from '@angular/router'
 import * as moment from 'moment'
 
+interface Sede{
+  id: number
+  nombre: String
+}
+
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent implements OnInit {
+  sedes: Sede[] = [
+    {id: 1, nombre: "Bosque"},
+    {id: 2, nombre: "Centro"},
+    {id: 3, nombre: "Estadio"},
+  ]
+
   user = {
     firstName: '',
     lastName: '',
     email: '',
     cuil: '',
     riesgo: '',
-    sede: '',
+    sede: this.sedes[1],
     password: '',
     role:1,
+
+
   } // 1 paciente, 2 vacunador, 3 admin
   // TODO cambiar esto para generar los primeros admins
 
@@ -25,6 +38,9 @@ export class RegisterPageComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  setSede(s: Sede){
+    this.user.sede = s;
+  }
   signUp() {
     this.authService.signUp(this.user).subscribe(
       (res) => {
