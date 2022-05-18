da8df0a28a2844db1591547a81685b2a86849020
Ian Petraccaro Cantero <ianpetraccaro@gmail.com>
2022-05-18 09:52:35 -0300

    Added responsive data to certificate

--- frontend/src/app/components/certificado/certificado.component.ts
+++ frontend/src/app/components/certificado/certificado.component.ts
@@ -1,6 +1,9 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import jsPDF from 'jspdf';
 import html2canvas from "html2canvas"
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-certificado',
@@ -15,15 +18,31 @@ export class CertificadoComponent implements OnInit {
   @ViewChild('htmlData') htmlData!:ElementRef;
 
   user={
-    nombre: "Pepe",
-    apellido: "Garcia",
-    cuil:"12-12345678-9",
-    vacunas:["covid 2 dosis", "gripe", "No tiene fiebre amarilla"]
+    nombre: '',
+    apellido: '',
+    cuil:'',
+    vacunas:['covid 2 dosis', 'gripe 1 dosis', 'No tiene fiebre amarilla']
   }
-  constructor() { }
+
+  private URL: string = environment.apiUrl
+  
+  constructor(private http: HttpClient, private authService: AuthService) {
+    this.http.get<any>(this.URL + '/user/' + this.authService.getId()).subscribe(
+      (res) => {
+        this.user.nombre = res.firstName
+        this.user.apellido = res.lastName
+        this.user.cuil = res.cuil
+      },
+      (err) => {
+        console.log(err)
+      }
+    )
+   }
+  
 
   public generatePDF() : void{
     let data: any = document.getElementById('htmlData');
+
     html2canvas(data).then((canvas) => {
       let fileWidth = 208;
       let fileHeight = (canvas.height * fileWidth) / canvas.width;
