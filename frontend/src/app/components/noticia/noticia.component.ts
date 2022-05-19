8f28c8900ccc4a275a1e71fcdb9fcf43e9ba0ec2
fots <facundotomatis@gmail.com>
2022-05-19 05:50:06 -0300

    frontend now is able to read, upload and delete articles

--- frontend/src/app/components/noticia/noticia.component.ts
+++ frontend/src/app/components/noticia/noticia.component.ts
@@ -1,36 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/services/auth.service';
+import { HttpClient } from '@angular/common/http'
+import { Component, OnInit } from '@angular/core'
+import { AuthService } from 'src/app/services/auth.service'
+import { environment } from 'src/environments/environment'
 
 interface Noticia {
-  titulo:String;
-  cuerpo:String
+  title: String
+  body: String
+  img: String
 }
 
 @Component({
   selector: 'app-noticia',
   templateUrl: './noticia.component.html',
-  styleUrls: ['./noticia.component.css']
+  styleUrls: ['./noticia.component.css'],
 })
 export class NoticiaComponent implements OnInit {
-  noticias:Noticia[] = [
-    {titulo:"Titulo 1", cuerpo:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident."},
-    {titulo:"Titulo 2", cuerpo:"Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.."},
-    {titulo:"Titulo 3", cuerpo:"Do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa."},
-    {titulo:"Titulo 4", cuerpo:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.."},
-    {titulo:"Titulo 5", cuerpo:"Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, mollit anim id est laborum.."},
-    {titulo:"Titulo 6", cuerpo:"Sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-  ]
+  private URL = environment.baseApiUrl + '/articles'
+  articles: Noticia[] = []
 
-  constructor(private authService:AuthService) { }
+  constructor(private authService: AuthService, private http: HttpClient) {
+    this.fetchData()
 
-  ngOnInit(): void {
   }
 
-  delete(): void {
+  ngOnInit(): void {}
+
+  private fetchData(){
+    this.articles=[]
+    this.http.get<any>(this.URL + '/getArticles').subscribe(
+      (res) => {
+        res.forEach((element: any) => {
+          const { title, img, body } = element
+          let obj = { title, img, body }
+          this.articles.push(obj)
+        })
+      },
+      (err) => {
+        console.log(err)
+      }
+    )
   }
 
-  isAdmin(): Boolean{
-    return this.authService.loggedIn() && this.authService.getRol()==3
+  delete(title: String): void {
+    this.http.delete(this.URL + '/deleteArticle/' + title).subscribe(
+      (res) => {
+        console.log(res)
+        this.fetchData()
+      },
+      (err) => {
+        console.log(err)
+      }
+    )
   }
 
+  isAdmin(): Boolean {
+    return this.authService.loggedIn() && this.authService.getRol() == 3
+  }
 }
--- frontend/src/app/components/nueva-noticia/nueva-noticia.component.ts
+++ frontend/src/app/components/nueva-noticia/nueva-noticia.component.ts
@@ -1,18 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
+import { environment } from 'src/environments/environment'
 
 @Component({
   selector: 'app-nueva-noticia',
   templateUrl: './nueva-noticia.component.html',
-  styleUrls: ['./nueva-noticia.component.css']
+  styleUrls: ['./nueva-noticia.component.css'],
 })
 export class NuevaNoticiaComponent implements OnInit {
-
-  titulo:String = ""
-  cuerpo:String = ""
-
-  constructor() { }
-
-  ngOnInit(): void {
+  private URL = environment.baseApiUrl + '/articles'
+  article = {
+    title: '',
+    img: '',
+    body: '',
   }
 
+  onFileChange(event: any) {
+    this.article.img = URL.createObjectURL(event.target.files[0])
+    const inputNode: any = document.querySelector('#file')
+    const reader = new FileReader()
+
+    reader.onload = (e: any) => {
+      const base64String = btoa(
+        String.fromCharCode(...new Uint8Array(e.target.result))
+      )
+      this.article.img = 'data:image/png;base64,' + base64String
+    }
+
+    reader.readAsArrayBuffer(inputNode.files[0])
+  }
+
+  constructor(private http: HttpClient,private router:Router) {}
+
+  uploadArticle() {
+    this.http.post<any>(this.URL + '/uploadArticle', this.article).subscribe(
+      (res) => {
+        this.router.navigate(["/Home"])
+      },
+      (err) => {
+        // TODO error mas amigable
+        console.log(err)
+      }
+    )
+  }
+
+  ngOnInit(): void {}
 }
