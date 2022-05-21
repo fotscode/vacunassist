923bba62ad79736d93af48f09d38c367473ef84f
fots <facundotomatis@gmail.com>
2022-05-21 07:05:23 -0300

    certificate now has the user vaccine data

--- frontend/src/app/components/certificado/certificado.component.ts
+++ frontend/src/app/components/certificado/certificado.component.ts
@@ -1,60 +1,85 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import jsPDF from 'jspdf';
-import html2canvas from "html2canvas"
-import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { AuthService } from 'src/app/services/auth.service';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'
+import jsPDF from 'jspdf'
+import html2canvas from 'html2canvas'
+import { environment } from 'src/environments/environment'
+import { HttpClient } from '@angular/common/http'
+import { AuthService } from 'src/app/services/auth.service'
 
 @Component({
   selector: 'app-certificado',
   templateUrl: './certificado.component.html',
-  styleUrls: ['./certificado.component.css']
+  styleUrls: ['./certificado.component.css'],
 })
-
-
-
 export class CertificadoComponent implements OnInit {
+  @ViewChild('htmlData') htmlData!: ElementRef
 
-  @ViewChild('htmlData') htmlData!:ElementRef;
-
-  user={
+  user = {
     nombre: '',
     apellido: '',
-    cuil:'',
-    vacunas:['covid 2 dosis', 'gripe 1 dosis', 'No tiene fiebre amarilla']
+    cuil: '',
+    vacunas: {
+      covid: {
+        nombre: 'Covid',
+        dosis: 0,
+      },
+      gripe: {
+        nombre: 'Gripe',
+        dosis: 0,
+      },
+      fiebreA: {
+        nombre: 'Fiebre Amarilla',
+        dosis: 0,
+      },
+    },
   }
 
-  private URL = environment.baseApiUrl +"/users"
-  
-  constructor(private http: HttpClient, private authService: AuthService) {
-    this.http.get<any>(this.URL + '/user/' + this.authService.getId()).subscribe(
-      (res) => {
-        this.user.nombre = res.firstName
-        this.user.apellido = res.lastName
-        this.user.cuil = res.cuil
-      },
-      (err) => {
-        console.log(err)
-      }
-    )
-   }
-  
+  private URL = environment.baseApiUrl + '/users'
+  private URLVaccines = environment.baseApiUrl + '/usersVaccines'
 
-  public generatePDF() : void{
-    let data: any = document.getElementById('htmlData');
+  constructor(private http: HttpClient, private authService: AuthService) {
+    this.http
+      .get<any>(this.URL + '/user/' + this.authService.getId())
+      .subscribe(
+        (res) => {
+          this.user.nombre = res.firstName
+          this.user.apellido = res.lastName
+          this.user.cuil = res.cuil
+        },
+        (err) => {
+          console.log(err)
+        }
+      )
+    this.http
+      .get<any>(this.URLVaccines + '/user/' + this.authService.getId())
+      .subscribe(
+        (res) => {
+          res.forEach((el: any) => {
+            Object.entries(this.user.vacunas).forEach(([k, v]) => {
+              if (k === el.vaccineId) {
+                v.dosis = el.doseNumber
+              }
+            })
+          })
+        },
+        (err) => {
+          console.log(err)
+        }
+      )
+  }
+
+  public generatePDF(): void {
+    let data: any = document.getElementById('htmlData')
 
     html2canvas(data).then((canvas) => {
-      let fileWidth = 208;
-      let fileHeight = (canvas.height * fileWidth) / canvas.width;
-      const FILEURI = canvas.toDataURL('image/png');
-      let PDF = new jsPDF('p', 'mm', 'a4');
-      let position = 0;
-      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
-      PDF.save('CertificadoVacunacion.pdf'); 
+      let fileWidth = 208
+      let fileHeight = (canvas.height * fileWidth) / canvas.width
+      const FILEURI = canvas.toDataURL('image/png')
+      let PDF = new jsPDF('p', 'mm', 'a4')
+      let position = 0
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight)
+      PDF.save('CertificadoVacunacion.pdf')
     })
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
