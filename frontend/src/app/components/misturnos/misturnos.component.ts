0f8cb513239eac19078aa8e4b615a13af0be4e07
fots <facundotomatis@gmail.com>
2022-06-03 02:32:50 -0300

    now home page is able to retreive own vaccines requests

--- frontend/src/app/components/misturnos/misturnos.component.ts
+++ frontend/src/app/components/misturnos/misturnos.component.ts
@@ -1,26 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Component, OnInit } from '@angular/core'
+import { AuthService } from 'src/app/services/auth.service'
+import { environment } from 'src/environments/environment'
+
+interface Vacuna {
+  _id: string
+  applied: boolean
+  createdAt: Date
+  dateIssued: number
+  dateApplied: number
+  dateConfirmed: number
+  doseNumber: number
+  modifiable: boolean
+  vaccineId: string
+}
+
+interface Turno {
+  estado: string
+  vacuna: string
+  fecha: string
+}
 
 @Component({
   selector: 'app-misturnos',
   templateUrl: './misturnos.component.html',
-  styleUrls: ['./misturnos.component.css']
+  styleUrls: ['./misturnos.component.css'],
 })
 export class MisturnosComponent implements OnInit {
+  private URL = environment.baseApiUrl + '/usersVaccines'
 
-  turnos=[{
-    id:'12',
-    vacuna:" covid",
-    fecha:" 10 de enero del 2022"
-  },
-  {
-    id:'15',
-    vacuna:"gripe",
-    fecha:" 20 de mayo del 2022"
-  }]
+  turnos: Array<Turno> = []
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private http: HttpClient, private authService: AuthService) {
+    this.http
+      .get<Array<Vacuna>>(this.URL + '/user/' + this.authService.getId())
+      .subscribe((res) => {
+        res
+          .filter((v) => !v.modifiable)
+          .forEach((v) => {
+            console.log(v)
+            let turno: Turno = {
+              estado: this.getEstado(v.dateConfirmed, v.applied),
+              vacuna: this.firstLetterUpper(v.vaccineId),
+              fecha: !v.dateConfirmed
+                ? 'No hay fecha'
+                : this.formatDate(new Date(v.dateConfirmed)),
+            }
+            this.turnos.push(turno)
+          })
+      })
+  }
+  private formatDate(d: Date): string {
+    let y = d.getFullYear()
+    let m = d.getMonth() + 1
+    let day = d.getDate()
+    return `${day}/${m}/${y}`
+  }
+  private firstLetterUpper(s: string): string {
+    return s.substring(0, 1).toUpperCase() + s.substring(1)
   }
 
+  private getEstado(dConfirmed: number, applied: boolean): string {
+    return !dConfirmed ? 'Pendiente' : !applied ? 'Confirmado' : 'Aplicado'
+  }
+  ngOnInit(): void {}
 }
