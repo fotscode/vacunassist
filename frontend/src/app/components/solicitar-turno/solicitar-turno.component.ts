ce1d3d6c3f8f17ac7014c9cbd469b905cbe31fbd
fots <facundotomatis@gmail.com>
2022-06-03 02:31:12 -0300

    refactor, added error messages and fixed other things

--- frontend/src/app/components/solicitar-turno/solicitar-turno.component.ts
+++ frontend/src/app/components/solicitar-turno/solicitar-turno.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http'
-import { Component, OnInit } from '@angular/core'
+import { Component, Inject, OnInit } from '@angular/core'
+import { MatSnackBar } from '@angular/material/snack-bar'
 import { AuthService } from 'src/app/services/auth.service'
 import { environment } from 'src/environments/environment'
 
@@ -7,7 +8,7 @@ interface Vacuna {
   _id: string
   applied: boolean
   createdAt: Date
-  dateIssued: Date
+  dateIssued: number
   dateApplied: number
   doseNumber: number
   modifiable: boolean
@@ -28,6 +29,7 @@ interface Approvable {
 })
 export class SolicitarTurnoComponent implements OnInit {
   private URL = environment.baseApiUrl + '/usersVaccines'
+  errorMsg: string = ''
   covid: Approvable = {
     maxDosage: false,
     validated: false,
@@ -44,37 +46,30 @@ export class SolicitarTurnoComponent implements OnInit {
   vacCovid: Vacuna | undefined
   vacGripe: Vacuna | undefined
 
-  constructor(private http: HttpClient, private authService: AuthService) {
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+    @Inject(MatSnackBar) private snackBar: MatSnackBar
+  ) {
     this.http
       .get<Array<Vacuna>>(this.URL + '/user/' + this.authService.getId())
       .subscribe((res) => {
         this.vacCovid = res.filter((e: Vacuna) => e.vaccineId == 'covid')[0]
-        this.setApprovableCovid(this.vacCovid)
+        this.setApprovable(this.vacCovid, this.covid, 2)
         this.vacGripe = res.filter((e: Vacuna) => e.vaccineId == 'gripe')[0]
-        this.setApprovableGripe(this.vacGripe)
-        // caso 10 del dia
+        this.setApprovable(this.vacGripe, this.gripe, 1)
       })
   }
 
   ngOnInit(): void {}
 
-  private setApprovableGripe(v: Vacuna) {
-    this.gripe.maxDosage = v.doseNumber < 1
+  private setApprovable(v: Vacuna, a: Approvable, doseMax: number) {
+    a.maxDosage = v.doseNumber < doseMax
     this.authService.getUser().subscribe((res) => {
-      this.gripe.validated = res.validated
+      a.validated = res.validated
     })
-    this.isBelowMaxCount().then((r) => (this.gripe.maxCount = r))
-
-    this.gripe.pending = !v.dateIssued
-  }
-
-  private setApprovableCovid(v: Vacuna) {
-    this.covid.maxDosage = v.doseNumber < 2
-    this.authService.getUser().subscribe((res) => {
-      this.covid.validated = res.validated
-    })
-    this.isBelowMaxCount().then((r) => (this.covid.maxCount = r))
-    this.covid.pending = !v.dateIssued
+    this.isBelowMaxCount().then((r) => (a.maxCount = r))
+    a.pending = !v.dateIssued
   }
 
   isApprovable(a: Approvable) {
@@ -90,7 +85,7 @@ export class SolicitarTurnoComponent implements OnInit {
               (v) =>
                 v.dateApplied == -1 &&
                 v.dateIssued &&
-                this.datesOnSameDay(v.dateIssued, new Date())
+                this.datesOnSameDay(new Date(v.dateIssued), new Date())
             ).length <= 10
           )
         } else resolve(false)
@@ -105,20 +100,31 @@ export class SolicitarTurnoComponent implements OnInit {
     )
   }
 
-  solicitarTurno(v:Vacuna | undefined){
-    if (v){
-      v.dateIssued = new Date()
+  solicitarTurno(v: Vacuna | undefined, a: Approvable) {
+    if (v && this.isApprovable(a)) {
+      v.dateIssued = new Date().getTime()
       v.modifiable = false
-      this.gripe.pending = false
+      a.pending = false
       this.http
         .put(this.URL + '/' + v._id, v)
         .toPromise()
         .then((res) => {
-          console.log(res)
+          this.snackBar.open(`Turno de ${v.vaccineId} solicitado`, void 0, { duration: 3000 })
         })
         .catch((err) => {
           console.log(err)
         })
+    } else if (v) {
+      this.errorMsg = this.getErrMsg(a) // TODO hacer mas lindo esto
     }
+  }
+  private getErrMsg(a: Approvable): string {
+    return !a.validated
+      ? 'No se encuentra validado'
+      : !a.pending
+      ? 'Ya tiene un turno pendiente'
+      : !a.maxCount
+      ? 'No hay mas dosis diarias de vacunas'
+      : 'Ya tiene la cantidad de dosis maximas'
   }
 }
